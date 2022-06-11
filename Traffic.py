6ac47a64b6c1de4f6df34357fba2b22bf6aa5a1f
julialruiter <julialruiter@gmail.com>
2022-06-11 15:52:04 +0200

    defining traffic manager fuctions

--- Traffic.py
+++ Traffic.py
@@ -1,16 +1,61 @@
 
 
 class TrafficManager:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def tick(self):
+        '''advance state of network'''
+        # tick_node
+        # tick_edge
+        # tick_car
+        pass
+
+    def get_snapshot(self):
+        '''outputs list of nodes, edges, car locations'''
+        pass
+
+    def get_node_neighbours(self):   # todo:  move to node
+        pass
+
+    def place_car(self, car):
+        pass
+
+    def remove_car(self, car):
+        pass
+
+    def advance_car(self, car):
+        pass
+
 
 class Network:
-    pass
+    def __init__(self) -> None:
+        pass
+
 
 class Node:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def tick(self):
+        '''advance state of network on the node level'''
+        # tick_edge
+        # tick_car
+        pass
 
 class Edge:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def tick(self):
+        '''advance state of network on the edge level'''
+        # tick_car
+        pass
 
 class Car:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def tick(self):
+        '''advance state of network on the car level'''
+        pass
